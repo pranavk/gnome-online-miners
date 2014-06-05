80376416dbf0b4158886a68581a14749a6476fba
Pranav Kant <pranav913@gmail.com>
2014-06-05 22:46:01 +0530

    re-arranged few functions here and there and fixed some more indentation

--- src/gom-dlna-server-device.c
+++ src/gom-dlna-server-device.c
@@ -67,30 +67,6 @@ G_DEFINE_TYPE_WITH_CODE (GomDlnaServerDevice, gom_dlna_server_device, G_TYPE_OBJ
   } G_STMT_END
 
 
-static void
-gom_dlna_server_device_dispose (GObject *object)
-{
-  GomDlnaServerDevice *self = GOM_DLNA_SERVER_DEVICE (object);
-  GomDlnaServerDevicePrivate *priv = self->priv;
-
-  g_clear_object (&priv->device);
-  g_clear_pointer (&priv->urls_to_item, g_hash_table_unref);
-
-  G_OBJECT_CLASS (gom_dlna_server_device_parent_class)->dispose (object);
-}
-
-
-static void
-gom_dlna_server_device_finalize (GObject *object)
-{
-  GomDlnaServerDevice *self = GOM_DLNA_SERVER_DEVICE (object);
-  GomDlnaServerDevicePrivate *priv = self->priv;
-
-  g_free (priv->well_known_name);
-  g_free (priv->object_path);
-
-  G_OBJECT_CLASS (gom_dlna_server_device_parent_class)->finalize (object);
-}
 
 
 static void
@@ -148,22 +124,25 @@ gom_dlna_server_device_set_property (GObject *object,
 }
 
 
-static void
-gom_dlna_server_device_proxy_new_cb (GObject *source_object,
-                                     GAsyncResult *res,
-                                     gpointer user_data)
+GomDlnaServerDevice*
+gom_dlna_server_device_new_for_bus_finish (GAsyncResult *result,
+                                           GError **error)
 {
-  GTask *init_task = G_TASK (user_data);
-  GomDlnaServerDevice *self;
-  GError *error = NULL;
+  GObject *object, *source_object;
+  GError *err = NULL;
 
-  self = GOM_DLNA_SERVER_DEVICE (g_task_get_source_object (init_task));
+  source_object = g_async_result_get_source_object (result);
+  object = g_async_initable_new_finish (G_ASYNC_INITABLE (source_object), result, &err);
+  g_object_unref (source_object);
 
-  self->priv->device = dleyna_server_media_device_proxy_new_for_bus_finish (res, &error);
-  RETURN_ON_ERROR (init_task, error, "Unable to load the MediaDevice interface");
+  if (err != NULL)
+    {
+      g_clear_object (&object);
+      g_propagate_error (error, err);
+      return NULL;
+    }
 
-  g_task_return_boolean (init_task, TRUE);
-  g_object_unref (init_task);
+  return GOM_DLNA_SERVER_DEVICE (object);
 }
 
 void
@@ -188,6 +167,129 @@ gom_dlna_server_device_new_for_bus (GBusType bus_type,
 }
 
 
+
+static void
+gom_dlna_server_device_get_icon_cb (GObject *source_object,
+                                    GAsyncResult *res,
+                                    gpointer user_data)
+{
+  GTask *task = G_TASK (user_data);
+  GInputStream *icon_stream;
+  GdkPixbuf *pixbuf;
+  GtkIconSize size;
+  gchar *icon_data;
+  gint height = -1;
+  gint width = -1;
+  gsize icon_data_size;
+  GError *error = NULL;
+
+  /* The icon data is forced to be a GVariant since the GDBus bindings
+   * assume bytestrings (type 'ay') to be nul-terminated and thus do
+   * not return the length of the buffer.
+   */
+  dleyna_server_media_device_call_get_icon_finish (DLEYNA_SERVER_MEDIA_DEVICE (source_object),
+                                                   &icon_data, NULL, res, &error);
+  RETURN_ON_ERROR (task, error, "Failed to call the GetIcon method");
+
+  size = (GtkIconSize) GPOINTER_TO_INT (g_task_get_task_data (task));
+  gtk_icon_size_lookup (size, &width, &height);
+  icon_data_size = sizeof (*icon_data);
+  
+  icon_stream = g_memory_input_stream_new_from_data (icon_data, icon_data_size, NULL);
+  pixbuf = gdk_pixbuf_new_from_stream_at_scale (icon_stream,
+                                                width,
+                                                height,
+                                                TRUE,
+                                                g_task_get_cancellable (task),
+                                                &error);
+  g_object_unref (icon_stream);
+
+  RETURN_ON_ERROR (task, error, "Failed to parse icon data");
+  g_task_return_pointer (task, pixbuf, g_object_unref);
+
+  g_object_unref (task);
+}
+
+GdkPixbuf *
+gom_dlna_server_device_get_icon_finish (GomDlnaServerDevice *self,
+                                        GAsyncResult *res,
+                                        GError **error)
+{
+  g_return_val_if_fail (g_task_is_valid (res, self), NULL);
+
+  return GDK_PIXBUF (g_task_propagate_pointer (G_TASK (res), error));
+}
+
+void
+gom_dlna_server_device_get_icon (GomDlnaServerDevice *self,
+                                 const gchar *requested_mimetype,
+                                 const gchar *resolution,
+                                 GtkIconSize size,
+                                 GCancellable *cancellable,
+                                 GAsyncReadyCallback callback,
+                                 gpointer user_data)
+{
+  GomDlnaServerDevicePrivate *priv = self->priv;
+  GTask *task;
+
+  task = g_task_new (self, cancellable, callback, user_data);
+  g_task_set_task_data (task, GINT_TO_POINTER (size), NULL);
+
+  dleyna_server_media_device_call_get_icon (priv->device, requested_mimetype, resolution,
+                                            cancellable, gom_dlna_server_device_get_icon_cb,
+                                            task);
+}
+
+const gchar *
+gom_dlna_server_device_get_object_path (GomDlnaServerDevice *self)
+{
+  return self->priv->object_path;
+}
+
+const gchar *
+gom_dlna_server_device_get_friendly_name (GomDlnaServerDevice *self)
+{
+  GomDlnaServerDevicePrivate *priv = self->priv;
+
+  return dleyna_server_media_device_get_friendly_name (priv->device);
+}
+
+const gchar *
+gom_dlna_server_device_get_udn (GomDlnaServerDevice *self)
+{
+  GomDlnaServerDevicePrivate *priv = self->priv;
+
+  return dleyna_server_media_device_get_udn (priv->device);
+}
+
+guint
+gom_dlna_server_device_get_shared_count (GomDlnaServerDevice *self)
+{
+  GomDlnaServerDevicePrivate *priv = self->priv;
+
+  return g_hash_table_size (priv->urls_to_item);
+}
+
+
+static void
+gom_dlna_server_device_proxy_new_cb (GObject *source_object,
+                                     GAsyncResult *res,
+                                     gpointer user_data)
+{
+  GTask *init_task = G_TASK (user_data);
+  GomDlnaServerDevice *self;
+  GError *error = NULL;
+
+  self = GOM_DLNA_SERVER_DEVICE (g_task_get_source_object (init_task));
+
+  self->priv->device = dleyna_server_media_device_proxy_new_for_bus_finish (res, &error);
+  RETURN_ON_ERROR (init_task, error, "Unable to load the MediaDevice interface");
+
+  g_task_return_boolean (init_task, TRUE);
+  g_object_unref (init_task);
+}
+
+
 static void
 gom_dlna_server_device_init_async (GAsyncInitable *initable,
                                    int io_priority,
@@ -203,12 +305,12 @@ gom_dlna_server_device_init_async (GAsyncInitable *initable,
   g_task_set_priority (init_task, io_priority);
 
   dleyna_server_media_device_proxy_new_for_bus (priv->bus_type,
-                                            G_DBUS_PROXY_FLAGS_NONE,
-                                            priv->well_known_name,
-                                            priv->object_path,
-                                            cancellable,
-                                            gom_dlna_server_device_proxy_new_cb,
-                                            init_task);
+                                                G_DBUS_PROXY_FLAGS_NONE,
+                                                priv->well_known_name,
+                                                priv->object_path,
+                                                cancellable,
+                                                gom_dlna_server_device_proxy_new_cb,
+                                                init_task);
 }
 
 
@@ -216,6 +318,31 @@ static void
 gom_dlna_server_device_async_initable_iface_init (GAsyncInitableIface *iface)
 {
   iface->init_async = gom_dlna_server_device_init_async;
+}
+
+static void
+gom_dlna_server_device_dispose (GObject *object)
+{
+  GomDlnaServerDevice *self = GOM_DLNA_SERVER_DEVICE (object);
+  GomDlnaServerDevicePrivate *priv = self->priv;
+
+  g_clear_object (&priv->device);
+  g_clear_pointer (&priv->urls_to_item, g_hash_table_unref);
+
+  G_OBJECT_CLASS (gom_dlna_server_device_parent_class)->dispose (object);
+}
+
+
+static void
+gom_dlna_server_device_finalize (GObject *object)
+{
+  GomDlnaServerDevice *self = GOM_DLNA_SERVER_DEVICE (object);
+  GomDlnaServerDevicePrivate *priv = self->priv;
+
+  g_free (priv->well_known_name);
+  g_free (priv->object_path);
+
+  G_OBJECT_CLASS (gom_dlna_server_device_parent_class)->finalize (object);
 }
 
 static void
@@ -304,132 +431,3 @@ gom_dlna_server_device_class_init (GomDlnaServerDeviceClass *class)
                                                         G_PARAM_STATIC_NICK));
 }
 
-
-GomDlnaServerDevice*
-gom_dlna_server_device_new_for_bus_finish (GAsyncResult *result,
-                                    GError **error)
-{
-  GObject *object, *source_object;
-  GError *err = NULL;
-
-  source_object = g_async_result_get_source_object (result);
-  object = g_async_initable_new_finish (G_ASYNC_INITABLE (source_object), result, &err);
-  g_object_unref (source_object);
-
-  if (err != NULL)
-    {
-      g_clear_object (&object);
-      g_propagate_error (error, err);
-      return NULL;
-    }
-
-  return GOM_DLNA_SERVER_DEVICE (object);
-}
-
-const gchar *
-gom_dlna_server_device_get_object_path (GomDlnaServerDevice *self)
-{
-  return self->priv->object_path;
-}
-
-static void
-gom_dlna_server_device_get_icon_cb (GObject *source_object,
-                                    GAsyncResult *res,
-                                    gpointer user_data)
-{
-  GTask *task = G_TASK (user_data);
-  GInputStream *icon_stream;
-  GdkPixbuf *pixbuf;
-  GtkIconSize size;
-  gchar *icon_data;
-  gint height = -1;
-  gint width = -1;
-  gsize icon_data_size;
-  GError *error = NULL;
-
-  /* The icon data is forced to be a GVariant since the GDBus bindings
-   * assume bytestrings (type 'ay') to be nul-terminated and thus do
-   * not return the length of the buffer.
-   */
-  dleyna_server_media_device_call_get_icon_finish (DLEYNA_SERVER_MEDIA_DEVICE (source_object),
-                                                   &icon_data, NULL, res, &error);
-  RETURN_ON_ERROR (task, error, "Failed to call the GetIcon method");
-
-  size = (GtkIconSize) GPOINTER_TO_INT (g_task_get_task_data (task));
-  gtk_icon_size_lookup (size, &width, &height);
-  icon_data_size = sizeof (*icon_data);
-  
-  icon_stream = g_memory_input_stream_new_from_data (icon_data, icon_data_size, NULL);
-  pixbuf = gdk_pixbuf_new_from_stream_at_scale (icon_stream,
-                                                width,
-                                                height,
-                                                TRUE,
-                                                g_task_get_cancellable (task),
-                                                &error);
-  g_object_unref (icon_stream);
-
-  RETURN_ON_ERROR (task, error, "Failed to parse icon data");
-  g_task_return_pointer (task, pixbuf, g_object_unref);
-
-  g_object_unref (task);
-}
-
-
-
-const gchar *
-gom_dlna_server_device_get_friendly_name (GomDlnaServerDevice *self)
-{
-  GomDlnaServerDevicePrivate *priv = self->priv;
-
-  return dleyna_server_media_device_get_friendly_name (priv->device);
-}
-
-
-const gchar *
-gom_dlna_server_device_get_udn (GomDlnaServerDevice *self)
-{
-  GomDlnaServerDevicePrivate *priv = self->priv;
-
-  return dleyna_server_media_device_get_udn (priv->device);
-}
-
-
-void
-gom_dlna_server_device_get_icon (GomDlnaServerDevice *self,
-                          const gchar *requested_mimetype,
-                          const gchar *resolution,
-                          GtkIconSize size,
-                          GCancellable *cancellable,
-                          GAsyncReadyCallback callback,
-                          gpointer user_data)
-{
-  GomDlnaServerDevicePrivate *priv = self->priv;
-  GTask *task;
-
-  task = g_task_new (self, cancellable, callback, user_data);
-  g_task_set_task_data (task, GINT_TO_POINTER (size), NULL);
-
-  dleyna_server_media_device_call_get_icon (priv->device, requested_mimetype, resolution,
-                                            cancellable, gom_dlna_server_device_get_icon_cb,
-                                            task);
-}
-
-
-GdkPixbuf *
-gom_dlna_server_device_get_icon_finish (GomDlnaServerDevice *self,
-                                        GAsyncResult *res,
-                                        GError **error)
-{
-  g_return_val_if_fail (g_task_is_valid (res, self), NULL);
-
-  return GDK_PIXBUF (g_task_propagate_pointer (G_TASK (res), error));
-}
-
-
-guint
-gom_dlna_server_device_get_shared_count (GomDlnaServerDevice *self)
-{
-  GomDlnaServerDevicePrivate *priv = self->priv;
-
-  return g_hash_table_size (priv->urls_to_item);
-}
