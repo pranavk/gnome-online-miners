a12e229268aea418f230cf3f0dd6c14d92f06c85
Pranav Kant <pranav913@gmail.com>
2014-06-20 13:02:49 +0530

    MediaServerMiner: Added support for searching non searchable devices.
    Fixes: https://bugzilla.gnome.org/show_bug.cgi?id=728912

--- src/gom-media-server-miner.c
+++ src/gom-media-server-miner.c
@@ -28,6 +28,7 @@
 #include "gom-media-server-miner.h"
 #include "gom-dlna-servers-manager.h"
 #include "gom-dlna-server.h"
+#include "gom-upnp-media-container2.h"
 
 #define MINER_IDENTIFIER "gd:media-server:miner:a4a47a3e-eb55-11e3-b983-14feb59cfa0e"
 
@@ -40,6 +41,7 @@ typedef struct {
   const gchar *url;
   const gchar *mimetype;
   const gchar *path;
+  const gchar *type;
 } PhotoItem;
 
 G_DEFINE_TYPE_WITH_PRIVATE (GomMediaServerMiner, gom_media_server_miner, GOM_TYPE_MINER)
@@ -168,25 +170,132 @@ generate_photo_item_from_variant (GVariant *var)
   g_variant_lookup (var, "Path", "o", &str);
   photo->path = g_strdup (str);
 
+  g_variant_lookup (var, "Type", "s", &str);
+  photo->type = g_strdup (str);
+
+  if (g_str_equal (photo->type, "container"))
+    {
+      photo->url = NULL;
+      goto out;
+    }
+
   g_variant_lookup (var, "URLs", "@as", &var1);
   g_variant_get_child (var1, 0, "s", &str);
   photo->url = g_strdup (str);
 
+ out:
   return photo;
+}
+
+/* Processes the variant, puts photos items in photos_list and return
+   a list of containers to be processed again. */
+static GList *
+process_variant (GVariant *out, GList **photos_list)
+{
+  GVariantIter *iter = NULL;
+  GVariant *var = NULL;
+  GList *containers = NULL;
+  PhotoItem *photo;
+  gchar *path = NULL;
+
+  g_variant_get (out, "aa{sv}", &iter);
+  while (g_variant_iter_loop (iter, "@a{sv}", &var))
+    {
+      photo = generate_photo_item_from_variant (var);
+      if (g_str_equal (photo->type, "image.photo"))
+        {
+          *photos_list = g_list_prepend (*photos_list, photo);
+        }
+      else if (g_str_equal (photo->type, "container"))
+        {
+          path = g_strdup (photo->path);
+          containers = g_list_prepend (containers, path);
+
+          g_slice_free (PhotoItem, photo);
+        }
+    }
+
+  return containers;
+}
+
+
+/*
+  Recursively find photo items in the directory structure.
+  Keep inserting photo items into Glist **photo_list as they
+  are found.
+*/
+static void
+find_photos (const gchar   *obj_path,
+             GList        **photos_list)
+{
+  UpnpMediaContainer2 *proxy = NULL;
+  const gchar *const filter[] = {"DisplayName","Type","Path", "URLs", "MIMEType"};
+  GVariant *var = NULL;
+  GError *error = NULL;
+  GList *containers = NULL, *tmp;
+
+  proxy = upnp_media_container2_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                                        G_DBUS_PROXY_FLAGS_NONE,
+                                                        "com.intel.dleyna-server",
+                                                        obj_path,
+                                                        NULL, /* GCancellable */
+                                                        &error);
+
+  if (error != NULL)
+    {
+      g_warning ("Unable to get proxy for Upnp.MediaContainer2 : %s",
+                 error->message);
+      g_error_free (error);
+      goto out;
+    }
+
+  upnp_media_container2_call_list_children_sync (proxy,
+                                                 0,
+                                                 0,
+                                                 filter,
+                                                 &var,
+                                                 NULL, /* GCancellable */
+                                                 &error);
+
+  if (error != NULL)
+    {
+      g_warning ("Unable to call ListChildren : %s",
+                 error->message);
+      g_error_free (error);
+      goto out;
+    }
+    
+  if (var == NULL)
+    goto out;
+
+  containers = process_variant (var, photos_list);
+  if (containers == NULL)
+    goto out;
+
+  for (tmp = containers; tmp != NULL; tmp = tmp->next)
+    {
+      find_photos (tmp->data, photos_list);
+    }
+  g_list_free_full (containers, g_free);
+
+ out:
+  g_variant_unref (var);
+  g_object_unref (proxy);
 }
 
 
 static GList *
-gom_media_server_get_photos (GObject *mngr,
+gom_media_server_get_photos (GObject     *mngr,
                              const gchar *udn)
 {
   GomDlnaServersManager *dlna_mngr = GOM_DLNA_SERVERS_MANAGER (mngr);
   GomDlnaServer *server = gom_dlna_servers_manager_get_server (dlna_mngr, udn);
   GList *photos_list = NULL;
   GError *error = NULL;
+  PhotoItem *photo;
   GVariant *out, *var;
   GVariantIter *iter = NULL;
-  PhotoItem *photo;
+  const gchar *obj_path;
 
   if (server == NULL)
     return NULL; /* Server is offline. */
@@ -213,7 +322,9 @@ gom_media_server_get_photos (GObject *mngr,
     }
   else
     {
-      /* TODO: Implement an algo here for !searchable devices. */
+      obj_path = gom_dlna_server_get_object_path (server);
+      /* This calls ListChildren recursively. */
+      find_photos (obj_path, &photos_list);
     }
 
   return photos_list;
@@ -225,21 +336,31 @@ query_media_server (GomAccountMinerJob *job,
 {
   GomMediaServerMiner *self = GOM_MEDIA_SERVER_MINER (job->miner);
   GomMediaServerMinerPrivate *priv = self->priv;
-  const gchar *udn;
+  const gchar *udn, *friendly_name;
   GError *local_error = NULL;
   GoaMediaServer *mediaserver;
   GList *photos_list, *tmp;
+  GomDlnaServer *server;
   GoaObject *object = GOA_OBJECT (g_hash_table_lookup (job->services, "photos"));
 
-  mediaserver = goa_object_peek_media_server (object);
-  udn = goa_media_server_get_udn (mediaserver);
-
-  photos_list = gom_media_server_get_photos (priv->mngr, udn);
+  mediaserver   = goa_object_peek_media_server (object);
+  udn           = goa_media_server_get_udn (mediaserver);
+  server        = gom_dlna_servers_manager_get_server (GOM_DLNA_SERVERS_MANAGER (priv->mngr),
+                                                       udn);
+  friendly_name = gom_dlna_server_get_friendly_name (server);
+  photos_list   = gom_media_server_get_photos (priv->mngr, udn);
   for (tmp = photos_list; tmp != NULL; tmp = tmp->next)
     {
-      account_miner_job_process_photo (job, tmp->data, "media_server", &local_error);
+      account_miner_job_process_photo (job, tmp->data, friendly_name, &local_error);
       g_slice_free (PhotoItem, tmp->data);
+      if (local_error != NULL)
+        {
+          g_warning ("Unable to process photo : %s",
+                     local_error->message);
+          g_error_free (local_error);
+        }
     }
+  g_object_unref (server);
 }
 
 static GHashTable *
